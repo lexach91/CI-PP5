70d7171b896f7bc42cd415d24ccd296887157daf
lexach91 <ronmctailor@protonmail.com>
2022-06-17 15:01:17 +0800

    Add rest_framework, corsheaders, and channels into installed apps in settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'channels',
 ]
 
 MIDDLEWARE = [
