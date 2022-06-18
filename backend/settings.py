8377a638b3cd54103571d6d42588a36c5d5c29f6
lexach91 <ronmctailor@protonmail.com>
2022-06-18 15:06:22 +0800

    Add cloudinary to installed apps

--- backend/settings.py
+++ backend/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
