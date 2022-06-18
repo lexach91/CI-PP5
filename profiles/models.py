2151ac2da0fe66772b47a482285d9678928e6216
lexach91 <ronmctailor@protonmail.com>
2022-06-18 15:06:41 +0800

    Add profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,29 @@
+import datetime
+from datetime import date
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Profile(models.Model):
+    """Profile model"""   
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    avatar = CloudinaryField(
+        'avatar',
+        folder='cloud_meetings_avatars',
+        null=True,
+        blank=True,
+    )
+    birth_date = models.DateField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    timezone = models.CharField(max_length=50)
+    profession = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    def age(self):
+        today = date.today()
+        return today.year - self.birth_date.year - ((today.month, today.day) < (self.birth_date.month, self.birth_date.day))
