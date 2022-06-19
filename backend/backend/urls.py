c99ce167a4fdf245340769fc92de86cdba1e9cb6
lexach91 <ronmctailor@protonmail.com>
2022-06-19 18:33:19 +0800

    Add urls for frontend and api apps

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('', include('frontend.urls')),
+    path('api/', include('api.urls')),
 ]
