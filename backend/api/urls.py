364a715030dce65a440d629cad75751d64c172ea
lexach91 <ronmctailor@protonmail.com>
2022-06-24 20:05:46 +0800

    Add api urls to register, login, logout, get user, and refresh token

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import CountriesService
+from authentication.views import LogoutAPIView, RegisterAPIView, LoginAPIView, UserAPIView, RefreshTokenAPIView
 
 
 urlpatterns = [
-    path('countries/', CountriesService.as_view(), name='countries'),
+    path('countries', CountriesService.as_view(), name='countries'),
+    path('register', RegisterAPIView.as_view(), name='register'),
+    path('login', LoginAPIView.as_view(), name='login'),
+    path('user', UserAPIView.as_view(), name='user'),
+    path('refresh', RefreshTokenAPIView.as_view(), name='refresh'),
+    path('logout', LogoutAPIView.as_view(), name='logout'),
 ]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,8 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('', include('frontend.urls')),
     path('api/', include('api.urls')),
+    path('', include('frontend.urls')),
 ]
