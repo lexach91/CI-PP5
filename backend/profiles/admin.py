9fd32d539d311975aaa7f5291d2bbd7aaeb54e13
lexach91 <ronmctailor@protonmail.com>
2022-06-24 19:57:42 +0800

    Remove profile model, create abstract user instead

--- backend/profiles/admin.py
+++ backend/profiles/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class AdminUser(admin.ModelAdmin):
+    list_display = ('id', 'email', 'first_name', 'last_name', 'is_staff', 'is_superuser', 'is_active', 'date_joined')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'date_joined')
+    search_fields = ('email', 'first_name', 'last_name')
+    readonly_fields = ('id', 'email', 'first_name', 'last_name', 'is_staff', 'is_superuser', 'is_active', 'date_joined')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+        ),
+    )
+    ordering = ('id',)
--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -1,25 +1,90 @@
 import datetime
 from datetime import date
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
-class Profile(models.Model):
-    """Profile model"""   
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, birth_date=None, first_name=None, last_name=None, country=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not password:
+            raise ValueError('Users must have a password')
+        if not birth_date:
+            raise ValueError('Users must have a birth date')
+        if not first_name:
+            raise ValueError('Users must have a first name')
+        if not last_name:
+            raise ValueError('Users must have a last name')
+        if not country:
+            raise ValueError('Users must have a country')
+        
+
+        user = self.model(
+            email=self.normalize_email(email),
+            birth_date=birth_date,
+            first_name=first_name,
+            last_name=last_name,
+            country=country,
+        )
+
+        user.set_password(password)
+        user.is_superuser = False
+        user.is_staff = False        
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password, birth_date, first_name, last_name, country):
+        print(email, password, birth_date, first_name, last_name, country)
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not password:
+            raise ValueError('Users must have a password')
+        if not birth_date:
+            raise ValueError('Users must have a birth date')
+        if not first_name:
+            raise ValueError('Users must have a first name')
+        if not last_name:
+            raise ValueError('Users must have a last name')
+        if not country:
+            raise ValueError('Users must have a country')
+        
+        user = self.model(
+            email=self.normalize_email(email),
+            birth_date=birth_date,
+            first_name=first_name,
+            last_name=last_name,
+            country=country,
+        )
+        user.set_password(password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractUser):
     avatar = CloudinaryField(
         'avatar',
         folder='cloud_meetings_avatars',
         null=True,
         blank=True,
     )
+    email = models.EmailField(max_length=254, unique=True)
+    username = None
     birth_date = models.DateField()
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    timezone = models.CharField(max_length=50)
-    profession = models.CharField(max_length=50)
+    timezone = models.CharField(max_length=50, blank=True, null=True)
+    profession = models.CharField(max_length=50, blank=True, null=True)
     country = models.CharField(max_length=50)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['birth_date', 'first_name', 'last_name', 'country']
+    
+    objects = UserManager()
     
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
@@ -27,3 +92,5 @@ class Profile(models.Model):
     def age(self):
         today = date.today()
         return today.year - self.birth_date.year - ((today.month, today.day) < (self.birth_date.month, self.birth_date.day))
+    
+
