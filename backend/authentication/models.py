3f70134f953993efc840aa12f77af477502d8b45
lexach91 <ronmctailor@protonmail.com>
2022-06-25 11:59:53 +0800

    Add model for forgot password token

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -7,3 +7,8 @@ class UserToken(models.Model):
     token = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     expired_at = models.DateTimeField()
+
+
+class ForgotPasswordToken(models.Model):
+    email = models.EmailField()
+    token = models.CharField(max_length=255)
