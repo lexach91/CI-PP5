ec460f9dc8aec989a43ea80a8be806a2fafe3750
lexach91 <ronmctailor@protonmail.com>
2022-06-30 19:55:42 +0800

    Remove refresh token interceptors from axios.js

--- frontend/src/interceptors/axios.js
+++ frontend/src/interceptors/axios.js
@@ -1,26 +1,4 @@
 import axios from "axios";
-// import {setToken} from "../redux/authSlice";
-
-
-
 
 axios.defaults.baseURL = "http://127.0.0.1:8080/api/";
 axios.defaults.withCredentials = true;
-
-
-
-
-let refreshing = false;
-
-axios.interceptors.response.use(resp => resp, async error => {
-    if (error.response.status === 401 && !refreshing) {
-        refreshing = true;
-        const response = await axios.post("refresh", {});
-
-        if (response.status === 200) {
-            return axios(error.config);
-        } 
-    }
-    refreshing = false;
-    return error;
-});
