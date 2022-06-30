490487c8da688065338b5c3bacbc3743979ce5b9
lexach91 <ronmctailor@protonmail.com>
2022-06-30 19:59:25 +0800

    Add code to choose between layouts in Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,24 +4,28 @@ import { useDispatch } from "react-redux";
 import { setAuth } from "../redux/authSlice";
 import { useSelector } from "react-redux";
 import { resetRedirect } from "../redux/authSlice";
+import { RotateLoader } from "react-spinners";
+import { VisitorLayout } from "../layouts/VisitorLayout";
+import { UserLayout } from "../layouts/UserLayout";
 
 
 const Home = () => {
     // const auth = useSelector(state => state.auth.isAuthenticated);
     // const user = useSelector(state => state.auth.user);
-    const { isAuthenticated, user } = useSelector(state => state.auth);
+    const { isAuthenticated, user, redirect, loading } = useSelector(state => state.auth);
     const [message, setMessage] = React.useState("");
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(resetRedirect());
-        
-        if (isAuthenticated) {
+        if (redirect) {
+            dispatch(resetRedirect());
+        }
+        if (isAuthenticated && user) {
             setMessage(`Welcome ${user.first_name} ${user.last_name}`);
         } else {
             setMessage("You are not logged in");
         }
-    }, [isAuthenticated, user]);
+    }, [isAuthenticated, user, redirect]);
 
 
     // useEffect(() => {
@@ -39,11 +43,35 @@ const Home = () => {
     //     })();
     // }, []);
     
-    return (
-        <div>
-        <h1>{message}</h1>
+    return loading ? (
+        <div
+          className="loader-container"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            width: "100vw",
+            backgroundColor: "#f5f5f5",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            zIndex: "9999",
+          }}>
+          <RotateLoader
+            sizeUnit={"px"}
+            size={150}
+            color={"#123abc"}
+            loading={loading}
+          />
         </div>
-    );
+      ) : (
+        isAuthenticated ? <UserLayout title='Home'>
+            <h1>{message}</h1>
+        </UserLayout> : <VisitorLayout title='Home'>
+            <h1>{message}</h1>
+        </VisitorLayout>
+      )
 };
 
 export default Home;
