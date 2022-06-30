5ad27c87c8604cd31cdc08df38d9f99baf08e4d4
lexach91 <ronmctailor@protonmail.com>
2022-06-30 19:57:01 +0800

    Add checkAuth function into App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import "primereact/resources/themes/mdc-dark-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
-import Navbar from './components/Navbar';
+import Navbar from './components/navigation/VisitorNavbar';
 import Home from './components/Home';
 import Register from './components/Register';
 import Login from './components/Login';
@@ -14,15 +14,26 @@ import {
   Route,
   Navigate,
 } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { checkAuth } from './redux/authSlice';
+import { useEffect } from 'react';
+import PrimeReact from 'primereact/api';
 
 
 
-function App() {
+
+const App = () => {
+  PrimeReact.ripple = true;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, []);
+
   return (
     <div className="App">
       
-      <Navbar />
-      <main>
         <Router>
           <Routes>
             <Route exact path="/" element={<Home />} />
@@ -32,7 +43,6 @@ function App() {
           </Routes>
         </Router>
 
-      </main>
     </div>
   );
 }
