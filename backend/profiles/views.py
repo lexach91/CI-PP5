1f856f83e8bc01f6fe8675d05772ada3f784f5e2
lexach91 <ronmctailor@protonmail.com>
2022-07-01 04:55:50 -0700

    Add api view to edit profile

--- backend/profiles/views.py
+++ backend/profiles/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from dj_rest_auth.registration.views import RegisterView
-from dj_rest_auth.views import LoginView
+from authentication.authentication import JWTAuthentication
+from .serializers import EditProfileSerializer
+
+
+class EditProfileAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    def post(self, request):
+        user = request.user
+        if user is None:
+            return Response({'error': 'You are not logged in'}, status=status.HTTP_401_UNAUTHORIZED)
+        new_data = request.data
+        serializer = EditProfileSerializer(user, data=new_data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
