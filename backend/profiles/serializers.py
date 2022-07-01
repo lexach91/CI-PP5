0bb8e29acb65e85e62eb9b4bf2b58fd977d3fd7c
lexach91 <ronmctailor@protonmail.com>
2022-07-01 04:54:15 -0700

    Add serializer to edit user's profile info

---
+++ backend/profiles/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework.serializers import ModelSerializer
+from .models import User
+
+
+class EditProfileSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'country',
+            'avatar',
+            'first_name',
+            'last_name',
+            'birth_date',
+        ]
