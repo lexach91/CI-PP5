95facf79e98b0ccdeb0915c71fefed223f5e5a1c
lexach91 <ronmctailor@protonmail.com>
2022-07-08 19:14:00 +0800

    Create videoroom model

--- backend/videorooms/models.py
+++ backend/videorooms/models.py
@@ -1,3 +1,37 @@
+import random, string
 from django.db import models
+from profiles.models import User
 
-# Create your models here.
+
+def create_room_token():
+    code = ''.join(random.choices(string.ascii_letters + string.digits, k=32))
+    if VideoRoom.objects.filter(token=code).exists():
+        return create_room_token()
+    return code
+
+
+class VideoRoom(models.Model):
+    token = models.CharField(max_length=32, default=create_room_token)
+    host = models.OneToOneField(User, on_delete=models.CASCADE, related_name='hosting_room')
+    max_guests = models.IntegerField(default=5)
+    guests = models.ManyToManyField(User, related_name='current_rooms')
+    screen_sharing_enabled = models.BooleanField(default=False)
+    presentation_enabled = models.BooleanField(default=False)
+    chat_enabled = models.BooleanField(default=False)
+    protected = models.BooleanField(default=False)
+    guests_input_control = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.host.first_name + ' ' + self.host.last_name + ' - ' + self.id
+    
+    def is_full(self):
+        return self.guests.count() >= self.max_guests
+    
+    def is_empty(self):
+        return self.guests.count() == 0
+    
+    def is_host(self, user):
+        return self.host == user
+    
+    def is_guest(self, user):
+        return self.guests.filter(id=user.id).exists()
