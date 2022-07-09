3db11a6d26ac40f1b1243c740c708112ea0df4bd
lexach91 <ronmctailor@protonmail.com>
2022-07-09 19:02:39 +0800

    Include videorooms urls to backend/urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('api/rooms/', include('videorooms.urls')),
     path('', include('frontend.urls')),
 ]
