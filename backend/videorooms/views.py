37acca42bc4317bcfda5701dd163f63f9646a20b
lexach91 <ronmctailor@protonmail.com>
2022-07-09 15:36:22 +0800

    Add join room api view

--- backend/videorooms/views.py
+++ backend/videorooms/views.py
@@ -20,3 +20,35 @@ class CreateRoomAPIView(APIView):
             serializer.save(host=user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class JoinRoomAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    
+    def post(self, request):
+        user = request.user
+        room_token = request.data['room_token']
+        if user is None:
+            return Response({'error': 'You are not logged in'}, status=status.HTTP_401_UNAUTHORIZED)
+        # check if room exists
+        if not VideoRoom.objects.filter(token=room_token).exists():
+            return Response({'error': 'Room does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        room = VideoRoom.objects.get(token=room_token)
+        
+        if room.host == user:
+            return Response({'error': 'You cannot join your own room'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if room.guests.filter(id=user.id).exists():
+            return Response({'error': 'You are already in this room'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if room.guests.count() >= room.max_guests:
+            return Response({'error': 'Room is full'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if room.protected:
+            if 'password' not in request.data:
+                return Response({'error': 'Room is protected'}, status=status.HTTP_400_BAD_REQUEST)
+            if not room.check_password(request.data['password']):
+                return Response({'error': 'Wrong password'}, status=status.HTTP_400_BAD_REQUEST)
+            
+        room.guests.add(user)
+        return Response({'success': 'You have joined the room'}, status=status.HTTP_200_OK)
