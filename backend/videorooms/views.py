4281f9c955d513baf54083b33914568ebbfaf7ae
lexach91 <ronmctailor@protonmail.com>
2022-07-09 15:35:47 +0800

    Add create room api view

--- backend/videorooms/views.py
+++ backend/videorooms/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import VideoRoomSerializer, VideoRoomMessageSerializer
+from authentication.authentication import JWTAuthentication
+from .models import VideoRoom, VideoRoomMessage
 
-# Create your views here.
+
+class CreateRoomAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+
+    def post(self, request):
+        user = request.user
+        if user is None:
+            return Response({'error': 'You are not logged in'}, status=status.HTTP_401_UNAUTHORIZED)
+        if VideoRoom.objects.filter(host=user).exists():
+            return Response({'error': 'You already have a room'}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = VideoRoomSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(host=user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
