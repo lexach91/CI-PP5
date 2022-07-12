d1f8d65c7fb1129d7e52c5e8b606899fd9369dab
lexach91 <ronmctailor@protonmail.com>
2022-07-12 18:54:00 +0800

    Add channels routing to asgi.py

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -13,4 +13,20 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-application = get_asgi_application()
+# application = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+import videorooms.routing
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                videorooms.routing.websocket_urlpatterns
+            )
+        )
+    ),
+})
