1012917c7163cf3a08dbacd77b024dfa89fb445b
lexach91 <ronmctailor@protonmail.com>
2022-07-18 10:53:03 +0800

    Change the order of imports in asgi.py

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -8,20 +8,20 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-# application = get_asgi_application()
-
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 import videorooms.routing
 
+# application = get_asgi_application()
+
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
