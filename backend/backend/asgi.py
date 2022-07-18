a50b6ac18321f143d0bef138700ca3d4a46b88e9
lexach91 <ronmctailor@protonmail.com>
2022-07-18 12:26:23 +0800

    Add django.setup() command to asgi.py

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -8,16 +8,18 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-
-from django.core.asgi import get_asgi_application
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
+
 
 # application = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
 import videorooms.routing
 
 application = ProtocolTypeRouter({
