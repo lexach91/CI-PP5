0bf0ca15291457eed2de121bf12d29767f8b807d
lexach91 <ronmctailor@protonmail.com>
2022-07-25 17:47:40 +0800

    Add url path to VideoRoomConsumer into routing.py

--- backend/videorooms/routing.py
+++ backend/videorooms/routing.py
@@ -1,8 +1,9 @@
-from django.urls import re_path
+from django.urls import re_path, path
 from .consumers import VideoRoomConsumer
 
 websocket_urlpatterns = [
     # room token is passed as a path parameter
     re_path(r'ws/videorooms/(?P<room_token>[^/]+)/$', VideoRoomConsumer.as_asgi()),
     re_path(r'ws/videorooms/(?P<room_token>\w+)/$', VideoRoomConsumer.as_asgi()),
+    path('ws/videorooms/<str:room_token>/', VideoRoomConsumer.as_asgi()),
 ]
