c71bdfcc6e7e237b05ed33e84c8e7dff4b5b7bcd
lexach91 <ronmctailor@protonmail.com>
2022-07-25 17:46:55 +0800

    Modify consumers.py to authenticate the user on connect

--- backend/videorooms/consumers.py
+++ backend/videorooms/consumers.py
@@ -5,64 +5,92 @@ from .models import VideoRoom, VideoRoomMessage
 from .serializers import VideoRoomSerializer, VideoRoomMessageSerializer
 from authentication.authentication import JWTAuthentication
 from profiles.models import User
+from channels.layers import get_channel_layer
+
+channel_layer = get_channel_layer()
 
 
 class VideoRoomConsumer(AsyncWebsocketConsumer):
     """
-    Consumer for video rooms
+    Consumer for video rooms, that handles the following:
+    - Connect to a video room using the token provided in the URL
+    - Send SDP offer to everyone in the room
+    - Receive SDP answer from everyone in the room
+    - Send ICE candidates to everyone in the room
+    - Receive ICE candidates from everyone in the room
+    - Send video room messages to everyone in the room
+    - Receive video room messages from everyone in the room
     """
     
     async def connect(self):
         """Connect to group websocket"""
-        print('connect')
-        # print request
-        print(self.scope)
-        self.user = self.scope['user']
         self.room_token = self.scope['url_route']['kwargs']['room_token']
-        self.room = await self.get_room()
-        print(self.room_token)
-        self.room_name = 'video_room_{}'.format(self.room_token)
+        self.room_group_name = 'videoroom_%s' % self.room_token
+        self.user = await self.get_user()
+        
         await self.channel_layer.group_add(
-            self.room_name,
+            self.room_group_name,
             self.channel_name
         )
+        
+        print('Connected to group %s' % self.room_group_name)
+        # print the request
+        print(self.scope)
+        print(self.user)
+        
         await self.accept()
         
-    @database_sync_to_async
-    def get_room(self):
-        """Get room by token"""
-        room = VideoRoom.objects.get(token=self.room_token)
-        print(room)
-        return room
-    
-    
     async def disconnect(self, close_code):
-        """Disconnect from group websocket"""
         await self.channel_layer.group_discard(
-            self.room_name,
+            self.room_group_name,
             self.channel_name
         )
-    
-    async def receive(self, text_data):
-        """Receive message from client"""
-        text_data_json = json.loads(text_data)
-        print(text_data_json)
-        print(self.user)
-        print(self.room)
-        await self.send_message(text_data_json)
         
-    async def send_message(self, message):
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        user_id = data['user_id']
+        action = data['action']
+        message = data['message']
+        
+        if action == 'sdp_offer' or action == 'sdp_answer':
+            receiver_channel_name = data['message']['receiver_channel_name']
+            
+            data['message']['receiver_channel_name'] = self.channel_name
+            
+            await self.channel_layer.group_send(
+                receiver_channel_name,
+                {
+                    'type': 'send_sdp',
+                    'data': data
+                }
+            )
+            
+            return
+        
+        data['message']['receiver_channel_name'] = self.channel_name
+        
         await self.channel_layer.group_send(
-            self.room_name,
+            self.room_group_name,
             {
-                'type': 'chat_message',
-                'message': message
+                'type': 'send_sdp',
+                'data': data
             }
         )
-
-    async def chat_message(self, event):
-        """Send message to client"""
-        message = event['message']
+        
+    async def send_sdp(self, event):
+        data = event['data']
+        
+        this_peer = data['peer']
+        action = data['action']
+        message = data['message']
+        
         await self.send(text_data=json.dumps({
+            'peer': this_peer,
+            'action': action,
             'message': message
         }))
+
+    @database_sync_to_async
+    def get_user(self):
+        user = JWTAuthentication.websocket_authenticate(self.scope)
+        return user
