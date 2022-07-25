ffadcd75f0eb04199227e8af113e081d4f762cac
lexach91 <ronmctailor@protonmail.com>
2022-07-25 17:48:46 +0800

    Add useEffect method to open websocket connection in Room.js component

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -7,162 +7,79 @@ import RotateLoader from "react-spinners/RotateLoader";
 import baseUrl from "../api/axios";
 import { useParams } from "react-router-dom";
 
-
 const Room = () => {
-    const {
-        loading,
-        redirect,
-        isAuthenticated,
-        user,
-    } = useSelector((state) => state.auth);
-    const dispatch = useDispatch();
-    const { roomToken } = useParams();
-    const [room, setRoom] = useState({});
-    const [error, setError] = useState("");
-    const messageRef = useRef();
-    const hostVideoRef = useRef();
-    const userVideoRef = useRef();
-    const userStreamRef = useRef();
-    const socketRef = useRef();
-    const peerRef = useRef();
-    const [guestsRefs, setGuestsRefs] = useState([]);
-    const [isHost, setIsHost] = useState(false);
-    // const [isLoading, setIsLoading] = useState(true);
-    // const [isMuted, setIsMuted] = useState(false);
-    // const [isStarted, setIsStarted] = useState(false);
-    // const [isEnded, setIsEnded] = useState(false);
+  const { loading, redirect, isAuthenticated, user } = useSelector(
+    (state) => state.auth
+  );
+  const dispatch = useDispatch();
+  const { roomToken } = useParams();
+  const [room, setRoom] = useState({});
+  const wsProtocol = window.location.protocol === "https:" ? "wss" : "ws";
+  const wsUrl = `${wsProtocol}://${window.location.host}/ws/videorooms/${roomToken}/`;
+  const webSocket = useRef();
 
-    useEffect(() => {
-        if (redirect) {
-            dispatch(resetRedirect());
-        }        
-    }, [redirect]);
+  useEffect(() => {
+    if (redirect) {
+      dispatch(resetRedirect());
+    }
+    if (isAuthenticated && user) {
+      const response = axios.get('api/rooms/get', {
+        params: {
+          room_token: roomToken,
+        },
+      });
+      setRoom(response.data);
+      }
+  }, [roomToken]);
 
-    const getRoomData = async () => {
-        let payload = {
-            room_token: roomToken,
-        };
-        console.log(payload);
-        console.log(roomToken);
-        // django will need to get rhe room token from the request data
-        // await axios.get
-        let res = await axios.get('rooms/get', {
-            params: payload,
-        });
-        console.log(res);
-        setRoom(res.data);
-        console.log(room);
-    };
+  useEffect(() => {
+    if (isAuthenticated && user) {
+      // open websocket connection sending cookies to the server
+      webSocket.current = new WebSocket(wsUrl);
+      webSocket.current.onopen = () => {
+        console.log("Connected to WebSocket");
+      }
+      webSocket.current.onmessage = (event) => {
+        console.log(event.data);
+      }
+      webSocket.current.onclose = () => {
+        console.log("Disconnected from WebSocket");
+      }
+    }
+  }, [room, isAuthenticated, user]);
 
-    const createPeer = async (userId) => {
-        let peer = new RTCPeerConnection({
-            iceServers: [
-                {
-                    urls: "stun:stun.stunprotocol.org",
-                },
-                {
-                    urls: "turn:numb.viagenie.ca",
-                    credential: "muazkh",
-                    username: "webrtc@live.com",
-                },
-            ],
-        });
-        //
-        peerRef.current = peer;
-        addTracksToPeer(peer);
-        }
+  return loading ? (
+    <div
+      className="loader-container"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "#f5f5f5",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        zIndex: "9999",
+      }}>
+      <RotateLoader
+        sizeUnit={"px"}
+        size={150}
+        color={"#123abc"}
+        loading={loading}
+      />
+    </div>
+  ) : isAuthenticated ? (
+    <div>
+      <h1>{room.id}</h1>
+    </div>
+  ) : (
+    <div>
+      <h1>You are not logged in</h1>
+    </div>
+  );
 
-    const addTracksToPeer = (peer) => {
-        userStreamRef.current.getTracks().forEach((track) => {
-            peer.addTrack(track, userStreamRef.current);
-        }
-        );
-    };
+};
 
-
-    const startStream = async () => {
-        const constraints = {
-            audio: true,
-            video: {
-                width: { ideal: 1280 },
-                height: { ideal: 720 },
-            },
-        };
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        if(isHost) {
-            hostVideoRef.current.srcObject = stream;
-            userStreamRef.current = stream;
-        } else {
-            userVideoRef.current.srcObject = stream;
-            userStreamRef.current = stream;
-        }
-        // socketRef.current = io.connect(`${baseUrl}/room/${roomToken}`);
-        // socketRef.current.emit("join room", {
-        //     room_token: roomToken,
-        //     user_id: user.id,
-        //     user_first_name: user.first_name,
-        //     user_last_name: user.last_name,
-        // });
-        // socketRef.current.on("user joined", (data) => {
-
-
-            
-    };
-
-    useEffect(() => {
-        if(isAuthenticated && roomToken) {
-            getRoomData();
-        }
-    }, [isAuthenticated, roomToken]);
-
-    useEffect(() => {
-        if(room) {
-            startStream();
-        }
-    }, [room]);
-
-
-    
-
-    return loading || !isAuthenticated ? (
-        <div
-          className="loader-container"
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh",
-            width: "100vw",
-            backgroundColor: "#f5f5f5",
-            position: "absolute",
-            top: 0,
-            left: 0,
-            zIndex: "9999",
-          }}>
-          <RotateLoader
-            sizeUnit={"px"}
-            size={150}
-            color={"#123abc"}
-            loading={loading}
-          />
-        </div>
-        ) : (
-            <div className="room-container">
-                <div className="videos-container">
-                    <div className="host-video">
-                        <video ref={hostVideoRef} autoPlay playsInline style={{
-                            width: "400px",
-                        }}/>
-                    </div>
-                    {/* {room.guests.map((guest) => (
-                        <div className="guest-video" key={guest.id}>
-                            <video autoPlay playsInline muted></video>
-                        </div>
-                    ))} */}
-                </div>
-            </div>
-        );
-
-}
-
-export default Room;        
+export default Room;
