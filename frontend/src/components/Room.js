361b262c2e3d379de376afe2ec1beebd3d16ad55
lexach91 <ronmctailor@protonmail.com>
2022-07-30 18:12:53 +0800

    Add functions to open websocket connection, create webrtc peers, and add local tracks to connection

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -7,6 +7,29 @@ import RotateLoader from "react-spinners/RotateLoader";
 import baseUrl from "../api/axios";
 import { useParams } from "react-router-dom";
 
+const servers = {
+  iceServers: [
+    {
+      urls: "stun:openrelay.metered.ca:80",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443?transport=tcp",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+  ],
+};
+
 const Room = () => {
   const { loading, redirect, isAuthenticated, user } = useSelector(
     (state) => state.auth
@@ -17,36 +40,202 @@ const Room = () => {
   const wsProtocol = window.location.protocol === "https:" ? "wss" : "ws";
   const wsUrl = `${wsProtocol}://${window.location.host}/ws/videorooms/${roomToken}/`;
   const webSocket = useRef();
+  const [isHost, setIsHost] = useState(false);
+  const [localStream, setLocalStream] = useState(null);
+  const [hostStream, setHostStream] = useState(null);
+  const [guests, setGuests] = useState({});
+  const [guestVideos, setGuestVideos] = useState([]);
 
   useEffect(() => {
     if (redirect) {
       dispatch(resetRedirect());
     }
     if (isAuthenticated && user) {
-      const response = axios.get('api/rooms/get', {
-        params: {
-          room_token: roomToken,
-        },
-      });
-      setRoom(response.data);
-      }
-  }, [roomToken]);
+      const response = axios
+        .get("rooms/get", {
+          params: {
+            room_token: roomToken,
+          },
+        })
+        .then((res) => {
+          setRoom(res.data);
+          if (res.data.host === user.id) {
+            setIsHost(true);
+            setHostStream(new MediaStream());
+          }
+          // setIsHost(res.data.host === user.id);
 
-  useEffect(() => {
-    if (isAuthenticated && user) {
-      // open websocket connection sending cookies to the server
-      webSocket.current = new WebSocket(wsUrl);
-      webSocket.current.onopen = () => {
-        console.log("Connected to WebSocket");
-      }
-      webSocket.current.onmessage = (event) => {
-        console.log(event.data);
-      }
-      webSocket.current.onclose = () => {
-        console.log("Disconnected from WebSocket");
-      }
+          webSocket.current = new WebSocket(wsUrl);
+          webSocket.current.onopen = () => {
+            console.log("Connected to WebSocket");
+            console.log(user);
+            console.log(roomToken);
+            console.log(room);
+          };
+          webSocket.current.onmessage = (event) => {
+            console.log(event.data);
+          };
+          webSocket.current.onclose = () => {
+            console.log("Disconnected from WebSocket");
+          };
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      // console.log(response);
+      // console.log(response.data);
+      // setRoom(response.data);
     }
-  }, [room, isAuthenticated, user]);
+  }, [roomToken, isAuthenticated, user]);
+
+  // useEffect(() => {
+  //   if (isAuthenticated && user) {
+  //     // open websocket connection sending cookies to the server
+  //     webSocket.current = new WebSocket(wsUrl);
+  //     webSocket.current.onopen = () => {
+  //       console.log("Connected to WebSocket");
+  //       console.log(user);
+  //       console.log(roomToken);
+  //       console.log(room);
+  //     }
+  //     webSocket.current.onmessage = (event) => {
+  //       console.log(event.data);
+  //     }
+  //     webSocket.current.onclose = () => {
+  //       console.log("Disconnected from WebSocket");
+  //     }
+  //   }
+  // }, [room, isAuthenticated, user]);
+
+  const sendSignal = (action, message) => {
+    let data = {
+      peer: user.id,
+      action: action,
+      message: message,
+    };
+    webSocket.current.send(JSON.stringify(data));
+  };
+
+  const wsOnMessage = (event) => {
+    let data = JSON.parse(event.data);
+    let peer = data.peer;
+    let action = data.action;
+    let message = data.message;
+
+    if (peer === user.id) {
+      return;
+    }
+
+    let receiver_channel_name = data.message.receiver_channel_name;
+
+    if (action === "new-peer") {
+      createOffer(peer, receiver_channel_name);
+    }
+  };
+
+  const wsOnOpen = () => {
+    console.log("Connected to WebSocket");
+    sendSignal("new-peer", {});
+  };
+
+  const createOffer = (peer, receiver_channel_name) => {
+    let peer_connection = new RTCPeerConnection(servers);
+
+    addLocalTracks(peer_connection);
+
+    let dataChannel = peer_connection.createDataChannel("data-channel");
+    dataChannel.onopen = () => {
+      console.log("data-channel open");
+    };
+    dataChannel.onclose = () => {
+      console.log("data-channel closed");
+    };
+    dataChannel.onmessage = (event) => {
+      console.log(event.data);
+      dcOnMessage(event);
+    };
+
+    let remoteVideo = createVideo(peer);
+    setOnTrack(peer_connection, remoteVideo);
+
+    setGuests((prevGuests) => {
+      return {
+        ...prevGuests,
+        [peer]: {
+          peer_connection,
+          dataChannel,
+        },
+      };
+    });
+
+    peer_connection.oniceconnectionstatechange = () => {
+      let iceConnectionState = peer_connection.iceConnectionState;
+
+      if (
+        iceConnectionState === "disconnected" ||
+        iceConnectionState === "failed"
+      ) {
+        console.log("iceConnectionState: " + iceConnectionState);
+        peer_connection.close();
+        deleteGuest(peer);
+        if (iceConnectionState !== "closed") {
+          peer_connection.close();
+        }
+      }
+    };
+  };
+
+  const addLocalTracks = (peer_connection) => {
+    if (isHost && hostStream) {
+      hostStream.getTracks().forEach((track) => {
+        peer_connection.addTrack(track, hostStream);
+      });
+      return;
+    }
+    if (localStream) {
+      localStream.getTracks().forEach((track) => {
+        peer_connection.addTrack(track, localStream);
+      });
+      return;
+    }
+  };
+
+  const dcOnMessage = (event) => {
+    let message = event.data;
+  };
+
+  const createVideo = (peer) => {
+    let video = document.createElement("video");
+    video.id = `video-${peer}`;
+    video.autoplay = true;
+    video.playsInline = true;
+
+    setGuestVideos((prevState) => [...prevState, video]);
+  };
+
+  const setOnTrack = (peer_connection, remoteVideo) => {
+    let remoteStream = new MediaStream();
+
+    remoteVideo.srcObject = remoteStream;
+    peer_connection.ontrack = async (event) => {
+      remoteStream.addTrack(event.track, remoteStream);
+    };
+  };
+
+  const deleteGuest = (peer) => {
+    setGuests((prevGuests) => {
+      return {
+        ...prevGuests,
+        [peer]: {
+          peer_connection: null,
+          dataChannel: null,
+        },
+      };
+    });
+    setGuestVideos((prevState) => {
+      return prevState.filter((video) => video.id !== `video-${peer}`);
+    });
+  };
 
   return loading ? (
     <div
@@ -70,16 +259,16 @@ const Room = () => {
         loading={loading}
       />
     </div>
-  ) : isAuthenticated ? (
+  ) : isAuthenticated && room ? (
     <div>
       <h1>{room.id}</h1>
+      <h2>{isHost ? "You are the host" : "You are not the host"}</h2>
     </div>
   ) : (
     <div>
       <h1>You are not logged in</h1>
     </div>
   );
-
 };
 
 export default Room;
