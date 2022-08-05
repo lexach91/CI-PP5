b4d03d063ed6f9ba2e615c47cd6802699bd4775e
lexach91 <ronmctailor@protonmail.com>
2022-08-05 17:37:36 +0800

    Fix bug when video was not appearing after reconnecting to a room

--- frontend/src/components/Video.js
+++ frontend/src/components/Video.js
@@ -1,27 +1,32 @@
-import {useRef, useEffect} from "react";
-
+import { useRef, useEffect } from "react";
 
 const VideoElement = (props) => {
-    const videoRef = useRef(null);
-    const { peer_connection} = props;
-    useEffect(() => {
-        if (peer_connection) {
-            peer_connection.ontrack = (event) => {
-                console.log("ontrack");
-                console.log(event);
-                videoRef.current.srcObject = event.streams[0];
-            }
-        }
-    }, [peer_connection]);
+  const videoRef = useRef(null);
+  const { peerConnection } = props;
 
-    return (
-        <video 
-            ref={videoRef}
-            autoPlay
-            playsInline
-            muted
-        />
-    );
+  useEffect(() => {
+    if(peerConnection._remoteStreams[0]) {
+        videoRef.current.srcObject = peerConnection._remoteStreams[0];
+    } else {
+        peerConnection.on("stream", (stream) => {
+            videoRef.current.srcObject = stream;
+        });
+        peerConnection.on("close", () => {
+            peerConnection.removeAllListeners();
+        } );
+        
+    }
+  }, [peerConnection]);
+
+  return (
+    <video
+      ref={videoRef}
+      autoPlay
+      playsInline
+      muted
+      className="video-element"
+    />
+  );
 };
 
 export default VideoElement;
