45cd130ce2cf792421072c0bbf41c61f4b650963
lexach91 <ronmctailor@protonmail.com>
2022-08-06 18:53:12 +0800

    Add basic layout for videoroom, functions to handle on/off for your mic and camera, mute all guests by host

--- frontend/src/components/Room3.js
+++ frontend/src/components/Room3.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { resetRedirect } from "../redux/authSlice";
 import RotateLoader from "react-spinners/RotateLoader";
 import { useParams } from "react-router-dom";
-import * as ReactDOM from 'react-dom';
+import * as ReactDOM from "react-dom";
 import VideoElement from "./Video";
 import Peer from "simple-peer";
 import { ContextMenu } from "primereact/contextmenu";
@@ -11,31 +11,31 @@ import { Button } from "primereact/button";
 import { Dock } from "primereact/dock";
 
 const servers = {
-    iceServers: [
-      {
-        urls: "stun:openrelay.metered.ca:80",
-      },
-      {
-        urls: "turn:openrelay.metered.ca:80",
-        username: "openrelayproject",
-        credential: "openrelayproject",
-      },
-      {
-        urls: "turn:openrelay.metered.ca:443",
-        username: "openrelayproject",
-        credential: "openrelayproject",
-      },
-      {
-        urls: "turn:openrelay.metered.ca:443?transport=tcp",
-        username: "openrelayproject",
-        credential: "openrelayproject",
-      },
-    ],
-  };
+  iceServers: [
+    {
+      urls: "stun:openrelay.metered.ca:80",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443?transport=tcp",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+  ],
+};
 
 const Room = () => {
   const { loading, redirect, isAuthenticated, user } = useSelector(
-      (state) => state.auth
+    (state) => state.auth
   );
   const dispatch = useDispatch();
   const { roomToken } = useParams();
@@ -48,16 +48,21 @@ const Room = () => {
   const [guests, setGuests] = useState([]);
   const guestsRef = useRef([]);
   const hostId = useRef();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [isFullScreen, setIsFullScreen] = useState(false);
 
   useEffect(() => {
     if (redirect) {
       dispatch(resetRedirect());
     }
     if (isAuthenticated && user) {
-      navigator.mediaDevices.getUserMedia({
-        audio: true,
-        video: true,
-      }).then((stream) => {
+      navigator.mediaDevices
+        .getUserMedia({
+          audio: true,
+          video: true,
+        })
+        .then((stream) => {
           webSocket.current = new WebSocket(wsUrl);
           localVideo.current.srcObject = stream;
           console.log(stream);
@@ -74,11 +79,20 @@ const Room = () => {
           webSocket.current.onerror = (error) => {
             console.log(error, "Websocket error");
           };
-      }).catch((error) => {
+        })
+        .catch((error) => {
           console.log(error);
-      });
+        });
     }
   }, [roomToken, isAuthenticated, user]);
+
+  // set isLoading to false when we have a hostId
+  useEffect(() => {
+    if (hostId.current) {
+      setIsLoading(false);
+      reloadLocalVideo();
+    }
+  }, [hostId.current]);
 
   const sendSignal = (action, message) => {
     let data = {
@@ -91,7 +105,7 @@ const Room = () => {
 
   const wsOnOpen = () => {
     console.log("Connected to WebSocket");
-  };  
+  };
 
   const wsOnMessage = (event) => {
     let data = JSON.parse(event.data);
@@ -102,16 +116,32 @@ const Room = () => {
       let room = data.room;
       setRoom(room);
       setIsHost(data.is_host);
+      hostId.current = room.host;
       let connectedPeers = data.connected_peers;
       Object.entries(connectedPeers).forEach(([peer, channelName]) => {
         console.log(peer, channelName);
-        if(peer != user.id) {
+        if (peer != user.id) {
           createOffer(peer, channelName);
         }
       });
       return;
     }
     let peer = data.peer;
+
+    if (action === "mute-all") {
+      console.log("mute-all");
+      if (!isHost) {
+        turnOffYourMic();
+      }
+      return;
+    }
+    if (action === "unmute-all") {
+      console.log("unmute-all");
+      if (!isHost) {
+        turnOnYourMic();
+      }
+      return;
+    }
     let message = data.message;
 
     if (peer == user.id) {
@@ -120,7 +150,6 @@ const Room = () => {
 
     let receiverChannelName = data.message.receiver_channel_name;
 
-    
     if (action === "new-offer") {
       let offer = message.sdp;
       createAnswer(offer, peer, receiverChannelName);
@@ -138,9 +167,9 @@ const Room = () => {
       console.log(guestsRef.current);
       console.log(guests);
     }
-    // if (action === "disconnected"){
-    //   deleteGuest(peer);
-    // }
+    if (action === "disconnected") {
+      deleteGuest(peer);
+    }
   };
 
   const createOffer = (peer, channelName) => {
@@ -155,22 +184,23 @@ const Room = () => {
         sdp: signal,
         receiver_channel_name: channelName,
       });
-    } );
+    });
 
     peerConnection.on("close", () => {
       console.log("peerConnection closed");
       deleteGuest(peer);
     });
 
-    
     let newGuest = {
       peer: peer,
       channelName: channelName,
       peerConnection: peerConnection,
       isHost: peer == hostId.current,
+      audioOn: true,
+      videoOn: true,
     };
     guestsRef.current.push(newGuest);
-    setGuests(prevGuests => [...prevGuests, newGuest]);
+    setGuests((prevGuests) => [...prevGuests, newGuest]);
     console.log(guestsRef.current);
     console.log(guests);
   };
@@ -187,17 +217,19 @@ const Room = () => {
         sdp: signal,
         receiver_channel_name: receiverChannelName,
       });
-    } );
-    peerConnection.signal(offer);
-    peerConnection.on("close", () => {
-      console.log("peerConnection closed");
-      deleteGuest(peer);
     });
+    peerConnection.signal(offer);
+    // peerConnection.on("close", () => {
+    //   console.log("peerConnection closed");
+    //   deleteGuest(peer);
+    // });
     let newGuest = {
       peer: peer,
       channelName: receiverChannelName,
       peerConnection: peerConnection,
       isHost: peer === hostId.current,
+      audioOn: false,
+      videoOn: false,
     };
     guestsRef.current.push(newGuest);
     setGuests((prevGuests) => [...prevGuests, newGuest]);
@@ -206,7 +238,9 @@ const Room = () => {
   };
 
   const deleteGuest = (peer) => {
-    let peerConnection = guestsRef.current.find((guest) => guest.peer == peer).peerConnection;
+    let peerConnection = guestsRef.current.find(
+      (guest) => guest.peer == peer
+    ).peerConnection;
     guestsRef.current = guestsRef.current.filter((guest) => guest.peer != peer);
     setGuests((prevGuests) => prevGuests.filter((guest) => guest.peer != peer));
     peerConnection.removeAllListeners();
@@ -215,51 +249,270 @@ const Room = () => {
     console.log(guests);
   };
 
-  const muteLocalSound = () => {
-    localVideo.current.srcObject.getAudioTracks()[0].enabled = false;
+  const reloadLocalVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+        audio: true,
+      })
+      .then((stream) => {
+        localVideo.current.srcObject = stream;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
-  const unmuteLocalSound = () => {
-    localVideo.current.srcObject.getAudioTracks()[0].enabled = true;
+  // function to mute/unmute sound of all guests
+  // this function can be used by the host only
+  // every guest will receive the same signal
+  const muteAllGuests = () => {
+    sendSignal("mute-all", {});
   };
 
-  const muteRemoteSound = (peer) => {
-    let guest = guestsRef.current.find((guest) => guest.peer === peer);
-    guest.peerConnection.getRemoteStreams()[0].getAudioTracks()[0].enabled = false;
+  const muteAllGuestsHandler = () => {
+    if (isHost) {
+      muteAllGuests();
+    }
   };
 
-  const unmuteRemoteSound = (peer) => {
-    let guest = guestsRef.current.find((guest) => guest.peer === peer);
-    guest.peerConnection.getRemoteStreams()[0].getAudioTracks()[0].enabled = true;
+  const unmuteAllGuests = () => {
+    sendSignal("unmute-all", {});
   };
 
+  const unmuteAllGuestsHandler = () => {
+    if (isHost) {
+      unmuteAllGuests();
+    }
+  };
 
+  const turnOnYourCamera = () => {
+    // let localVideoTrack = localVideo.current.srcObject.getVideoTracks()[0];
+    guestsRef.current.forEach((guest) => {
+      guest.peerConnection.streams[0].getVideoTracks().forEach((track) => {
+        track.enabled = true;
+      });
+    });
+    localVideo.current.srcObject.getVideoTracks().forEach((track) => {
+      track.enabled = true;
+    });
+  };
 
-  return (
-    <div className="room">
-      <div className="room-header">
-        <h1>{room.id}</h1>
-        <h2>{isHost ? "You are host" : "You are guest"}</h2>
+  const turnOffYourCamera = () => {
+    // let localVideoTrack = localVideo.current.srcObject.getVideoTracks()[0];
+    guestsRef.current.forEach((guest) => {
+      guest.peerConnection.streams[0].getVideoTracks().forEach((track) => {
+        track.enabled = false;
+      });
+    });
+    localVideo.current.srcObject.getVideoTracks().forEach((track) => {
+      track.enabled = false;
+    });
+  };
+
+  const turnOnYourMic = () => {
+    // let localAudioTrack = localVideo.current.srcObject.getAudioTracks()[0];
+    guestsRef.current.forEach((guest) => {
+      guest.peerConnection.streams[0].getAudioTracks().forEach((track) => {
+        track.enabled = true;
+      });
+    });
+    localVideo.current.srcObject.getAudioTracks().forEach((track) => {
+      track.enabled = true;
+    });
+  };
+  const turnOffYourMic = () => {
+    // let localAudioTrack = localVideo.current.srcObject.getAudioTracks()[0];
+    guestsRef.current.forEach((guest) => {
+      guest.peerConnection.streams[0].getAudioTracks().forEach((track) => {
+        track.enabled = false;
+      });
+    });
+    localVideo.current.srcObject.getAudioTracks().forEach((track) => {
+      track.enabled = false;
+    });
+  };
+
+  // return isLoading ? (
+  //     <div className="loading">
+  //       <video
+  //         ref={localVideo}
+  //         autoPlay
+  //         playsInline
+  //         muted
+  //         className="video-element"
+  //       />
+  //       <div className="loading-text">
+  //         <h1>
+  //           Loading... <i className="pi pi-spinner pi-spin"></i>
+  //         </h1>
+  //       </div>
+  //     </div>
+
+  //     ) : (
+  //   <div className="room">
+  //     <div className="room-header">
+  //       <h1>{room.id}</h1>
+  //       <h2>{isHost ? "You are host" : "You are guest"}</h2>
+  //     </div>
+  //     <div className="room-body">
+  //       <video
+  //         ref={localVideo}
+  //         autoPlay
+  //         playsInline
+  //         muted
+  //         className="video-element"
+  //       />
+  //       {guests.map((guest, index) => {
+  //         return (
+  //           <VideoElement
+  //             key={index}
+  //             peerConnection={guest.peerConnection}
+  //           />
+  //         );
+  //       })}
+  //     </div>
+  //   </div>
+  // );
+  return isLoading ? (
+    <div className="loading">
+      <video
+        ref={localVideo}
+        autoPlay
+        playsInline
+        muted
+        className="video-element"
+      />
+      <div className="loading-text">
+        <h1>
+          Loading... <i className="pi pi-spinner pi-spin"></i>
+        </h1>
       </div>
-      <div className="room-body">
-        <video
-          ref={localVideo}
-          autoPlay
-          playsInline
-          muted
-          className="video-element"
-        />
-        {guests.map((guest, index) => {
-          return (
-            <VideoElement
-              key={index}
-              peerConnection={guest.peerConnection}
-            />
-          );
-        })}
+    </div>
+  ) : (
+    <div
+      className="room"
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        width: "100%",
+        height: "100%",
+      }}>
+      <div className="grid grid-nogutter">
+        <div className="col-fixed room-controls" style={{ width: "100px" }}>
+          {isHost ? (
+            <>
+              <Button
+                label="Turn on your camera"
+                icon="pi pi-video"
+                onClick={turnOnYourCamera}
+              />
+              <Button
+                label="Turn off your camera"
+                icon="pi pi-video"
+                onClick={turnOffYourCamera}
+              />
+              <Button
+                label="Turn on your mic"
+                icon="pi pi-microphone"
+                onClick={turnOnYourMic}
+              />
+              <Button
+                label="Turn off your mic"
+                icon="pi pi-microphone"
+                onClick={turnOffYourMic}
+              />
+              <Button
+                label="Mute all guests"
+                icon="pi pi-volume-off"
+                onClick={muteAllGuestsHandler}
+              />
+              <Button
+                label="Unmute all guests"
+                icon="pi pi-volume-up"
+                onClick={unmuteAllGuestsHandler}
+              />
+            </>
+          ) : (
+            <>
+              <Button
+                label="Turn on your camera"
+                icon="pi pi-video"
+                onClick={turnOnYourCamera}
+              />
+              <Button
+                label="Turn off your camera"
+                icon="pi pi-video"
+                onClick={turnOffYourCamera}
+              />
+              <Button
+                label="Turn on your mic"
+                icon="pi pi-microphone"
+                onClick={turnOnYourMic}
+              />
+              <Button
+                label="Turn off your mic"
+                icon="pi pi-microphone"
+                onClick={turnOffYourMic}
+              />
+            </>
+          )}
+        </div>
+        <div className="col grid grid-nogutter">
+          <div className="host-video-container col-6 md:col-4 lg:col-3">
+            {isHost ? (
+              <video
+                ref={localVideo}
+                autoPlay
+                playsInline
+                muted
+                className="video-element"
+              />
+            ) : (
+              guests.map((guest, index) => {
+                if (guest.isHost) {
+                  return (
+                    <VideoElement
+                      key={index}
+                      peerConnection={guest.peerConnection}
+                    />
+                  );
+                }
+              })
+            )}
+          </div>
+          {!isHost ? (
+            <div className="guest-video-container col-6 md:col-4 lg:col-3">
+              <video
+                ref={localVideo}
+                autoPlay
+                playsInline
+                muted
+                className="video-element"
+              />
+            </div>
+          ) : (
+            <></>
+          )}
+          {guests.map((guest, index) => {
+            if (!guest.isHost) {
+              return (
+                <div className="guest-video-container col-6 md:col-4 lg:col-3">
+                  <VideoElement
+                    key={index}
+                    peerConnection={guest.peerConnection}
+                  />
+                </div>
+              );
+            }
+          })}
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default Room;
--- frontend/src/components/Video.js
+++ frontend/src/components/Video.js
@@ -4,28 +4,26 @@ const VideoElement = (props) => {
   const videoRef = useRef(null);
   const { peerConnection } = props;
 
+  
+
   useEffect(() => {
-    if(peerConnection._remoteStreams[0]) {
-        videoRef.current.srcObject = peerConnection._remoteStreams[0];
+    if (peerConnection._remoteStreams[0]) {
+      videoRef.current.srcObject = peerConnection._remoteStreams[0];
     } else {
-        peerConnection.on("stream", (stream) => {
-            videoRef.current.srcObject = stream;
-        });
-        peerConnection.on("close", () => {
-            peerConnection.removeAllListeners();
-        } );
-        
+      peerConnection.on("stream", (stream) => {
+        videoRef.current.srcObject = stream;        
+      });
     }
   }, [peerConnection]);
 
   return (
-    <video
-      ref={videoRef}
-      autoPlay
-      playsInline
-      muted
-      className="video-element"
-    />
+      <video
+        ref={videoRef}
+        autoPlay
+        playsInline
+        // muted
+        className="video-element"
+      />        
   );
 };
 
