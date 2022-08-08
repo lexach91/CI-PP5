9b349be1246de8e717843500c3275d92c90db346
lexach91 <ronmctailor@protonmail.com>
2022-08-08 17:17:14 +0800

    Add Payment and PaymentHistory models in admin.py

--- backend/payments/admin.py
+++ backend/payments/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Payment, PaymentHistory
 
-# Register your models here.
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('membership', 'is_complete', 'is_canceled', 'created_at', 'updated_at')
+    
+@admin.register(PaymentHistory)
+class PaymentHistoryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'Amount_of_payments')
+    
+    def Amount_of_payments(self, obj):
+        return obj.payments.count()
