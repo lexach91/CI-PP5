56e56d70f39358d5c3424413cfebcefb1a700d36
lexach91 <ronmctailor@protonmail.com>
2022-08-08 17:16:39 +0800

    Create Payment and PaymentHistory models in payments app

--- backend/payments/models.py
+++ backend/payments/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from subscriptions.models import SubscriptionPlan, Membership
+from profiles.models import User
 
-# Create your models here.
+
+class Payment(models.Model):
+    membership = models.ForeignKey(Membership, on_delete=models.CASCADE, related_name='payments')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_complete = models.BooleanField(default=False)
+    is_canceled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.membership.user.email
+
+class PaymentHistory(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='payment_history')
+    payments = models.ManyToManyField(Payment, related_name='payment_history')
+    
+    def __str__(self):
+        return self.user.email
