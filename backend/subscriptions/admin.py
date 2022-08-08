76b2071896ecc1283589c0bafaf930acabf54362
lexach91 <ronmctailor@protonmail.com>
2022-08-08 17:15:57 +0800

    Add SubscriptionPlan and Membership models in admin.py

--- backend/subscriptions/admin.py
+++ backend/subscriptions/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import SubscriptionPlan, Membership
 
-# Register your models here.
+
+@admin.register(SubscriptionPlan)
+class SubscriptionPlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'can_create_rooms', 'price', 'guest_limit', 'description', 'created_at', 'updated_at')
+    
+
+@admin.register(Membership)
+class MembershipAdmin(admin.ModelAdmin):
+    list_display = ('user', 'is_active', 'type', 'opened_at', 'renewed_at', 'expires_at')
