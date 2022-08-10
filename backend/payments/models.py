726fc99f63f1527e78d866e2356992414e4a591c
lexach91 <ronmctailor@protonmail.com>
2022-08-10 17:14:25 +0800

    Add amount field to payment model and change str method

--- backend/payments/models.py
+++ backend/payments/models.py
@@ -6,12 +6,10 @@ from profiles.models import User
 class Payment(models.Model):
     membership = models.ForeignKey(Membership, on_delete=models.CASCADE, related_name='payments')
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    is_complete = models.BooleanField(default=False)
-    is_canceled = models.BooleanField(default=False)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
     def __str__(self):
-        return self.membership.user.email
+        return str(self.amount) + ' on ' + str(self.created_at.date())
 
 class PaymentHistory(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='payment_history')
