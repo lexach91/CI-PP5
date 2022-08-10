e73ecd87765d4b9bcde622ca784292763582d95e
lexach91 <ronmctailor@protonmail.com>
2022-08-10 17:16:07 +0800

    Change create checkout session view to check if user has membership, add cases to stripe webhook listener

--- backend/payments/views.py
+++ backend/payments/views.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+# import datetime
 import json
 from django.shortcuts import render
 from rest_framework.views import APIView
@@ -8,6 +9,8 @@ import stripe
 from django.conf import settings
 from authentication.authentication import JWTAuthentication
 from subscriptions.models import SubscriptionPlan, Membership
+from profiles.models import User
+from .models import Payment, PaymentHistory
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 # set up webhook key
@@ -17,6 +20,7 @@ base_url = settings.BASE_URL
 
 class CreateCheckoutSessionView(APIView):
     authentication_classes = [JWTAuthentication]
+
     def post(self, request):
         user = request.user
         if user is None:
@@ -27,26 +31,38 @@ class CreateCheckoutSessionView(APIView):
             return Response({'error': 'Plan not found'}, status=status.HTTP_404_NOT_FOUND)
         if plan.price == 0:
             return Response({'error': 'Free plan'}, status=status.HTTP_400_BAD_REQUEST)
+        user_membership = Membership.objects.get(user=user)
+        if user_membership is not None and user_membership.type.name != 'Free' and (user_membership.expires_at - datetime.now()).days > 0:
+            return Response({'error': 'You already have a membership'}, status=status.HTTP_400_BAD_REQUEST)                
+        # create checkout session
+        customer = stripe.Customer.list(email=user.email)
+        print(customer)
+        if len(customer.data) == 0:
+            customer = stripe.Customer.create(email=user.email)
         else:
-            # create checkout session
-            try:
-                session = stripe.checkout.Session.create(
-                    payment_method_types=['card'],
-                    subscription_data={
-                        'items': [{
-                            'plan': plan.stripe_plan_id,
-                        }],
-                    },
-                    success_url=base_url + '/checkout?success=true&session_id={CHECKOUT_SESSION_ID}',
-                    cancel_url=base_url + '/checkout?cancelled=true',
-                )
-            except Exception as e:
-                return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-            return Response({'sessionId': session['id']}, status=status.HTTP_200_OK)
-        
+            customer = customer.data[0]
+        try:
+            session = stripe.checkout.Session.create(
+                payment_method_types=['card'],
+                subscription_data={
+                    'items': [{
+                        'plan': plan.stripe_plan_id,
+                    }],
+                },
+                customer=customer.id,
+                # customer_email=user.email,
+                success_url=base_url +
+                '/checkout?success=true&session_id={CHECKOUT_SESSION_ID}',
+                cancel_url=base_url + '/checkout?cancelled=true',
+            )
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({'sessionId': session['id']}, status=status.HTTP_200_OK)
+
 
 class CheckoutSessionView(APIView):
     authentication_classes = [JWTAuthentication]
+
     def post(self, request):
         user = request.user
         if user is None:
@@ -60,30 +76,66 @@ class CheckoutSessionView(APIView):
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         return Response({'session': session}, status=status.HTTP_200_OK)
-    
+
 
 class StripeWebhookListener(APIView):
+    # @csrf_exempt
     def post(self, request):
         webhook_secret = settings.STRIPE_WEBHOOK_KEY
-        # request_data = json.loads(request.data)
-        
-        if webhook_secret:
-            signature = request.headers.get('stripe-signature')
-            try:
-                event = stripe.Webhook.construct_event(
-                    payload=json.dumps(request.data), sig_header=signature, secret=webhook_secret)
-                data = event['data']
-            except Exception as e:
-                return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        payload = request.body
+        event = None
+
+        try:
+            event = stripe.Event.construct_from(
+                json.loads(payload), webhook_secret)
+            # data = event['data']
+            # print(data, 'data')
+        except Exception as e:
+            print(e, 'error')
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+        # Handle the event        
+        if event.type == 'customer.subscription.created':
+            subscription = event.data.object
+            customer_id = subscription.customer
+            customer = stripe.Customer.retrieve(customer_id)
+            user = User.objects.get(email=customer.email)
+            plan = SubscriptionPlan.objects.get(stripe_plan_id=subscription["items"]["data"][0]["plan"]["id"])
+            membership = Membership.objects.get_or_create(user=user)[0]
+            membership.type = plan
+            membership.expires_at = datetime.now() + timedelta(days=31 if datetime.now().month%2 == 0 else 30)
+            membership.stripe_id = subscription.id
+            membership.save()
+            payment_history = PaymentHistory.objects.get_or_create(user=user)[0]
+            payment = Payment.objects.create(membership=membership, amount=plan.price)
+            payment_history.payments.add(payment)
+            payment_history.save()
+        if event.type == 'customer.subscription.updated':
+            subscription = event.data.object
+            customer_id = subscription.customer
+            customer = stripe.Customer.retrieve(customer_id)
+            user = User.objects.get(email=customer.email)
+            plan = SubscriptionPlan.objects.get(stripe_plan_id=subscription["items"]["data"][0]["plan"]["id"])
+            membership = Membership.objects.get(user=user)
+            membership.type = plan
+            membership.expires_at = datetime.now() + timedelta(days=31)
+            membership.stripe_id = subscription.id
+            membership.save()
+            payment_history = PaymentHistory.objects.get_or_create(user=user)[0]
+            payment = Payment.objects.create(membership=membership, amount=plan.price)
+            payment_history.payments.add(payment)
+            payment_history.save()
+        if event.type == 'customer.subscription.deleted':
+            subscription = event.data.object
+            customer_id = subscription.customer
+            customer = stripe.Customer.retrieve(customer_id)
+            user = User.objects.get(email=customer.email)
+            membership = Membership.objects.get(user=user)
+            free_plan = SubscriptionPlan.objects.get(name='Free')
+            membership.type = free_plan
+            membership.expires_at = None
+            membership.stripe_id = None
+            membership.save()
             
-            event_type = event['type']
-            
-        else:
-            data = request.data
-            event_type = data['type']
-            
-        data_object = data['object']
-        
-        print('event type: ' + event_type)
-        
-        return Response({'received': 'ok'}, status=status.HTTP_200_OK)
+        return Response({'received': True}, status=status.HTTP_200_OK)
