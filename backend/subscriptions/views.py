89a06dc90f9cdc1a6bd0de18860c94604754b85b
lexach91 <ronmctailor@protonmail.com>
2022-08-11 18:41:23 +0800

    Add api view to get user's membership plan

--- backend/subscriptions/views.py
+++ backend/subscriptions/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import SubscriptionPlan, Membership
+from .serializers import SubscriptionPlanSerializer
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from authentication.authentication import JWTAuthentication
 
-# Create your views here.
+
+
+class GetUsersSubscriptionPlanAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    def get(self, request):
+        user = request.user
+        if user is None:
+            return Response({'error': 'You are not logged in'}, status=status.HTTP_401_UNAUTHORIZED)
+        membership = Membership.objects.get_or_create(user=user)[0]
+        plan = membership.type
+        serializer = SubscriptionPlanSerializer(plan)
+        return Response(serializer.data, status=status.HTTP_200_OK)
