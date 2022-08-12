40d0ae9b10377a0389e0176f6164e16cea518d05
lexach91 <ronmctailor@protonmail.com>
2022-08-12 13:47:26 +0800

    Add allowed hosts to settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', 'django-react-video-meetings.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
