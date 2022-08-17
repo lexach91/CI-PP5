b734a8a50051d606e71805abbc7d1999c63892ce
lexach91 <ronmctailor@protonmail.com>
2022-08-17 13:09:36 +0800

    Change Menubar to Toolbar in UserNavbar.js and add home button

--- frontend/src/components/navigation/UserNavbar.js
+++ frontend/src/components/navigation/UserNavbar.js
@@ -11,116 +11,176 @@ import { Toast } from "primereact/toast";
 import { Card } from "primereact/card";
 import { Avatar } from "primereact/avatar";
 import { Divider } from "primereact/divider";
-
+import { Toolbar } from "primereact/toolbar";
 
 const UserNavbar = () => {
-    const { 
-        isAuthenticated,
-        user,
-        error,
-        loading,
-        redirect,
-        membershipLoading,
-        membership,
-     } = useSelector(state => state.auth);
-    const dispatch = useDispatch();
-    const [sidebarVisible, setSidebarVisible] = useState(false);
-    const toast = useRef(null);
+  const {
+    isAuthenticated,
+    user,
+    error,
+    loading,
+    redirect,
+    membershipLoading,
+    membership,
+  } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const [sidebarVisible, setSidebarVisible] = useState(false);
+  const toast = useRef(null);
 
-    useEffect(() => {
-        if (error) {
-            toast.current.show({
-                severity: 'error',
-                summary: 'Error',
-                detail: error,
-                life: 7000
-            });
-            dispatch(logout());
-        }
-    }, [error]);
+  useEffect(() => {
+    if (error) {
+      toast.current.show({
+        severity: "error",
+        summary: "Error",
+        detail: error,
+        life: 7000,
+      });
+      dispatch(logout());
+    }
+  }, [error]);
 
-    
+  const logoutButton = (
+    <React.Fragment>
+      <div className="p-menuitem">
+        <Button
+          label="Logout"
+          icon="pi pi-power-off"
+          className="p-button-danger"
+          onClick={async () => {
+            await dispatch(logout());
+            window.location.href = "/";
+          }}
+        />
+      </div>
+    </React.Fragment>
+  );
 
-    const logoutButton = (
-        <React.Fragment>
-            <div className="p-menuitem">
-                <Button label="Logout" icon="pi pi-power-off" className="p-button-danger" onClick={async () => {
-                    await dispatch(logout());
-                    window.location.href = "/";
-
-                } } />
-            </div>
-        </React.Fragment>
-    );
-
-    const userAvatar = () => {
-        if (user) {
-            if (user.avatar) {
-                return <Chip image={user.avatar} label={user.first_name + " " + user.last_name} />;
-            } else {
-                return <Chip label={user.first_name + " " + user.last_name} icon="pi pi-user" />;
+  const userAvatar = () => {
+    if (user) {
+      if (user.avatar) {
+        return (
+          <Chip
+            image={user.avatar}
+            label={
+              window.innerWidth > 600
+                ? user.first_name + " " + user.last_name
+                : user.first_name
             }
-        }
-    };
+          />
+        );
+      } else {
+        return (
+          <Chip
+            label={
+              window.innerWidth > 600
+                ? user.first_name + " " + user.last_name
+                : user.first_name
+            }
+            icon="pi pi-user"
+          />
+        );
+      }
+    }
+  };
 
-    const userButton = (
-        <React.Fragment>
-            <div className="p-menuitem" onClick={
-                () => {
-                    setSidebarVisible(true);
-                }
-            }>
-                {userAvatar()}
-            </div>
-        </React.Fragment>
-    );
+  const homeButton = (
+    <React.Fragment>
+      <div className="p-menuitem">
+        <Button
+          label="Home"
+          icon="pi pi-home"
+          className="p-button-secondary ml-3"
+          onClick={() => {
+            window.location.href = "/";
+          }}
+        />
+      </div>
+    </React.Fragment>
+  );
 
-    return isAuthenticated && user ? (
-        <header className="navbar">
-            <Toast ref={toast} />
-            <nav className="navbar-nav">
-                <Menubar start={userButton} end={logoutButton} />
-            </nav>
-            <Sidebar visible={sidebarVisible} onHide={() => setSidebarVisible(false)} style={{width: "fit-content"}}>
-                <div className="p-grid">
-                    <div className="p-col-12">
-                        <Card>
-                            <div className="flex">
-                                <div className="p-col-4 flex justify-center align-center">
-                                    <Avatar size="xlarge" image={user.avatar ? user.avatar : "https://res.cloudinary.com/lexach91/image/upload/v1653724113/lklqlcqgl2pklvdi9rbt.svg"} />
-                                </div>
-                                <Divider layout="vertical" />
-                                <div className="p-col-8">
-                                    <h5>{user.first_name + " " + user.last_name}</h5>
-                                    <p>{user.email}</p>
-                                </div>
-                            </div>
-                        </Card>
-                    </div>
-                    <div className="p-col-12">
-                        <Button label="Profile" icon="pi pi-user" className="p-button-secondary mb-2 w-full" onClick={() => {
-                            window.location.href = '/profile'
-                        } } />
-                    </div>
-                    <div className="p-col-12">
-                        <Button label="Settings" icon="pi pi-cog" className="p-button-secondary mb-2 w-full" onClick={() => {
-                            window.location.href = '/settings'
-                        } } />
-                    </div>
-                    {!membershipLoading && membership && membership.can_create_rooms && (
-                    <div className="p-col-12">
-                        <Button label="Create Room" icon="pi pi-plus" className="p-button-secondary mb-2 w-full" onClick={() => {
-                            window.location.href = '/create-room'
-                        } } />
-                    </div>
-
-                    )}
+  const userButton = (
+    <React.Fragment>
+      <div
+        className="p-menuitem"
+        onClick={() => {
+          setSidebarVisible(true);
+        }}
+        style={{ cursor: "pointer", userSelect: "none" }}>
+        {userAvatar()}
+      </div>
+      {homeButton}
+    </React.Fragment>
+  );
+  return isAuthenticated && user ? (
+    <header className="navbar">
+      <Toast ref={toast} />
+      <nav className="navbar-nav">
+        <Toolbar left={userButton} right={logoutButton} />
+      </nav>
+      <Sidebar
+        visible={sidebarVisible}
+        onHide={() => setSidebarVisible(false)}
+        style={{ width: "fit-content" }}>
+        <div className="p-grid">
+          <div className="p-col-12">
+            <Card>
+              <div className="flex">
+                <div className="p-col-4 flex justify-center align-center">
+                  <Avatar
+                    size="xlarge"
+                    image={
+                      user.avatar
+                        ? user.avatar
+                        : "https://res.cloudinary.com/lexach91/image/upload/v1653724113/lklqlcqgl2pklvdi9rbt.svg"
+                    }
+                  />
                 </div>
-            </Sidebar>
-        </header>
-    ) : (
-        <>      </>
-    );
-}
+                <Divider layout="vertical" />
+                <div className="p-col-8">
+                  <h5>{user.first_name + " " + user.last_name}</h5>
+                  <p>{user.email}</p>
+                </div>
+              </div>
+            </Card>
+          </div>
+          <div className="p-col-12">
+            <Button
+              label="Profile"
+              icon="pi pi-user"
+              className="p-button-secondary mb-2 w-full"
+              onClick={() => {
+                window.location.href = "/profile";
+              }}
+            />
+          </div>
+          <div className="p-col-12">
+            <Button
+              label="Settings"
+              icon="pi pi-cog"
+              className="p-button-secondary mb-2 w-full"
+              onClick={() => {
+                window.location.href = "/settings";
+              }}
+            />
+          </div>
+          {!membershipLoading && membership && membership.can_create_rooms && (
+            <div className="p-col-12">
+              <Button
+                label="Create Room"
+                icon="pi pi-plus"
+                className="p-button-secondary mb-2 w-full"
+                onClick={() => {
+                  window.location.href = "/create-room";
+                }}
+              />
+            </div>
+          )}
+        </div>
+      </Sidebar>
+    </header>
+  ) : (
+    <> </>
+  );
+};
 
 export default UserNavbar;
