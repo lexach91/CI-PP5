4bdaf00fa26307f09586e038653c873a70614ee1
lexach91 <ronmctailor@protonmail.com>
2022-08-18 19:41:11 +0800

    Change code in CountryService.js to use new data

--- frontend/src/service/CountryService.js
+++ frontend/src/service/CountryService.js
@@ -4,6 +4,6 @@ export class CountryService {
     getCountries() {
         const url = window.location.origin + '/api/countries';
         return fetch(url).then(res => res.json())
-            .then(d => d.data);
+            .then(d => d);
     }
 }
