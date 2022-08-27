0b514f9372f1712e47888ca9fc597a30186fdbcc
lexach91 <ronmctailor@protonmail.com>
2022-08-27 18:57:42 +0800

    Create unsubscribe from newsletter view

--- backend/contactus/views.py
+++ backend/contactus/views.py
@@ -32,3 +32,18 @@ class NewsletterSubscriptionAPIView(APIView):
         NewsletterSubscription.objects.create(email=email)        
 
         return Response(status=status.HTTP_201_CREATED)
+
+
+class UnsubscribeNewsletterAPIView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        
+        if not email:
+            return Response({"error": "Please provide all the required fields"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if not NewsletterSubscription.objects.filter(email=email).exists():
+            return Response({"error": f"{email} are not subscribed to our newsletter"}, status=status.HTTP_400_BAD_REQUEST)
+
+        NewsletterSubscription.objects.get(email=email).delete()       
+
+        return Response(status=status.HTTP_200_OK)
