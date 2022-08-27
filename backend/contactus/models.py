decb2f6d90f7fedf370495d084264d5fa670bf39
lexach91 <ronmctailor@protonmail.com>
2022-08-27 15:49:41 +0800

    Add contact us django app, and views for contacting and subscribing

---
+++ backend/contactus/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+
+
+class ContactUs(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+    
+    def __str__(self):
+        return self.name + ' - ' + self.subject
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        # Send email to the user
+        subject = 'Thank you for contacting us'
+        html_message = render_to_string('contactus/contactus_email.html', {'name': self.name, 'subject': self.subject})
+        plain_message = strip_tags(html_message)
+        
+        send_mail(
+            subject=subject,
+            message=plain_message,
+            from_email="dr.meetings@hotmail.com",
+            recipient_list=[self.email],
+            html_message=html_message,
+        )
+
+
+class NewsletterSubscription(models.Model):
+    email = models.EmailField()
+    
+    def __str__(self):
+        return self.email
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        # Send email to the user
+        subject = 'Thank you for subscribing to our newsletter'
+        html_message = render_to_string('contactus/newsletter_email.html')
+        plain_message = strip_tags(html_message)
+        
+        send_mail(
+            subject=subject,
+            message=plain_message,
+            from_email="dr.meetings@hotmail.com",
+            recipient_list=[self.email],
+            html_message=html_message,
+        )
---
+++ backend/contactus/views.py
@@ -0,0 +1,34 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import ContactUs, NewsletterSubscription
+
+
+class ContactUsAPIView(APIView):
+    def post(self, request):
+        name = request.data.get('name')
+        email = request.data.get('email')
+        subject = request.data.get('subject')
+        message = request.data.get('message')
+        
+        if not name or not email or not subject or not message:
+            return Response({"error": "Please provide all the required fields"}, status=status.HTTP_400_BAD_REQUEST)
+
+        ContactUs.objects.create(name=name, email=email, subject=subject, message=message)        
+
+        return Response(status=status.HTTP_201_CREATED)
+    
+
+class NewsletterSubscriptionAPIView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        
+        if not email:
+            return Response({"error": "Please provide all the required fields"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if NewsletterSubscription.objects.filter(email=email).exists():
+            return Response({"error": "You are already subscribed to our newsletter"}, status=status.HTTP_400_BAD_REQUEST)
+
+        NewsletterSubscription.objects.create(email=email)        
+
+        return Response(status=status.HTTP_201_CREATED)
