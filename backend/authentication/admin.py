f34e42709e1b39b6e9e02c1314c2e57b5841c012
lexach91 <ronmctailor@protonmail.com>
2022-08-28 14:30:25 +0800

    Remove imports for UserToken in authentication/admin.py

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -1,15 +1,6 @@
 from django.contrib import admin
-from .models import UserToken, ForgotPasswordToken
+from .models import ForgotPasswordToken
 # Register your models here.
-
-
-@admin.register(UserToken)
-class UserTokenAdmin(admin.ModelAdmin):
-    list_display = ('user_id', 'token', 'created_at', 'expired_at')
-    list_filter = ('user_id', 'created_at', 'expired_at')
-    search_fields = ('user_id', 'created_at', 'expired_at')
-    ordering = ('user_id', 'created_at', 'expired_at')
-    readonly_fields = ('user_id', 'token', 'created_at', 'expired_at')
     
     
 
