9ec9044ead7f8a83f71d2b7579f9fcd0e79c0948
lexach91 <ronmctailor@protonmail.com>
2022-08-28 14:29:26 +0800

    Remove UserToken model as redundant

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,13 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
-class UserToken(models.Model):
-    user_id = models.IntegerField()
-    token = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    expired_at = models.DateTimeField()
-
 
 class ForgotPasswordToken(models.Model):
     email = models.EmailField()
