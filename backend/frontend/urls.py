77a31017332450c3784b1d180ffec9c6856dd738
lexach91 <ronmctailor@protonmail.com>
2022-08-29 18:10:41 +0800

    Add robots.txt and sitemap.xml to urls not served by react

--- backend/frontend/urls.py
+++ backend/frontend/urls.py
@@ -3,5 +3,5 @@ from .views import index
 
 urlpatterns = [
     path("", index, name="index"),
-    re_path(r"^(?!api/|api|admin/|admin).*", index),
+    re_path(r"^(?!api/|api|admin/|admin|robots.txt/|robots.txt|sitemap.xml/|sitemap.xml).*", index),
 ]
