63ec049ca0f2d1d58692c437fd52212c2fc89a5a
lexach91 <ronmctailor@protonmail.com>
2022-08-29 16:47:46 +0800

    Format python code to meet pep8 requirements in frontend app

--- backend/frontend/urls.py
+++ backend/frontend/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, re_path
 from .views import index
 
 urlpatterns = [
-    path('', index, name='index'),
-    re_path(r'^(?!api/|api|admin/|admin).*', index),
+    path("", index, name="index"),
+    re_path(r"^(?!api/|api|admin/|admin).*", index),
 ]
