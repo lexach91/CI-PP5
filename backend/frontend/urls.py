a12c1ce0dd9151f5c1e4d37ea2a6624a49633e35
lexach91 <ronmctailor@protonmail.com>
2022-08-30 16:15:15 +0800

    Fix pep8 validator errors in frontend app

--- backend/frontend/urls.py
+++ backend/frontend/urls.py
@@ -3,5 +3,8 @@ from .views import index
 
 urlpatterns = [
     path("", index, name="index"),
-    re_path(r"^(?!api/|api|admin/|admin|robots.txt/|robots.txt|sitemap.xml/|sitemap.xml).*", index),
+    re_path(
+        r"^(?!api/|api|admin/|admin|robots.txt/|robots.txt|sitemap.xml/|sitemap.xml).*",
+        index,
+    ),
 ]
