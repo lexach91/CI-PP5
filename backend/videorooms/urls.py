f2c23068ca5792ee2540b0667afd31b911aa0821
lexach91 <ronmctailor@protonmail.com>
2022-08-30 18:15:39 +0800

    Fix repeated names in videorooms/urls.py

--- backend/videorooms/urls.py
+++ backend/videorooms/urls.py
@@ -16,9 +16,9 @@ urlpatterns = [
     path("join", JoinRoomAPIView.as_view(), name="join"),
     path("leave", LeaveRoomAPIView.as_view(), name="leave"),
     path("delete", DeleteRoomAPIView.as_view(), name="delete"),
-    path("set-password", SetPasswordAPIView.as_view(), name="set-password"),
+    path("set-password", SetPasswordAPIView.as_view(), name="set-room-password"),
     path(
-        "reset-password", ResetPasswordAPIView.as_view(), name="reset-password"
+        "reset-password", ResetPasswordAPIView.as_view(), name="reset-room-password"
     ),
     path(
         "check-user-in-room",
