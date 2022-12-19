d1a3eb9861efcc39f2bc11e01324ec31e32b8e24
lexach91 <ronmctailor@protonmail.com>
2022-12-19 17:00:13 +0700

    Change commands order in build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -o errexit
+cd frontend && npm install && npm run build && npm run postbuild && npm run sitemap && cd ..
 pip install -r requirements.txt
 python backend/manage.py collectstatic --noinput
 python backend/manage.py makemigrations && python backend/manage.py migrate
-cd frontend && npm install && npm run build && npm run postbuild && npm run sitemap && cd ..
