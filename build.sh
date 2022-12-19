4b74b1c90d7ea2f16141f4148a06e854542bd781
lexach91 <ronmctailor@protonmail.com>
2022-12-19 16:24:48 +0700

    Fix wrong path in build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -o errexit
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py makemigrations && python manage.py migrate
+python backend/manage.py collectstatic --noinput
+python backend/manage.py makemigrations && python backend/manage.py migrate
 cd frontend && npm install && npm run build && npm run postbuild && npm run sitemap && cd ..
