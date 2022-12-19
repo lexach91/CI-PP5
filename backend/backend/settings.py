fa0e185bfb238a56ff792d1ab8380437512af4e0
lexach91 <ronmctailor@protonmail.com>
2022-12-19 16:09:55 +0700

    Add render hostname to allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,8 +37,11 @@ DEBUG = os.environ.get("DEBUG") == "True"
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    "django-react-video-meetings.herokuapp.com",
 ]
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
