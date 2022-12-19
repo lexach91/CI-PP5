78e3508abf70809b1139cad75f4947bfce500513
lexach91 <ronmctailor@protonmail.com>
2022-12-19 17:12:24 +0700

    Add npm cache cleaning command to build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -o errexit
-cd frontend && npm install && npm run build && npm run postbuild && npm run sitemap && cd ..
+cd frontend && npm cache clean --force && rm -rf node_modules && rm -rf package-lock.json && npm install && npm run build && cd ..
 pip install -r requirements.txt
 python backend/manage.py collectstatic --noinput
 python backend/manage.py makemigrations && python backend/manage.py migrate
