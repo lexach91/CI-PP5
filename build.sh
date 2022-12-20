296c9c8685fdac5c7d83ae62d1b4c7a986f5ae20
lexach91 <ronmctailor@protonmail.com>
2022-12-20 14:06:06 +0700

    Add prebuild command for frontend in build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -o errexit
-cd frontend && rm -rf node_modules && rm -rf package-lock.json && npm cache clean --force && npm install && npm run build
-cd .. && pip install -r requirements.txt
+npm run heroku-prebuild
+pip install -r requirements.txt
 python backend/manage.py collectstatic --noinput
 python backend/manage.py makemigrations && python backend/manage.py migrate
